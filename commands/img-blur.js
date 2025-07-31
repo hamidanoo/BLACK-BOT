9d9f5585718494d4abfb3a4cf584630d69fe3dce
Hamid Shah <133199761+hamidanoo@users.noreply.github.com>
2025-07-31 20:20:53 +0500

    Commit

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -65,8 +65,8 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363302677217436@newsletter',
-                    newsletterName: 'JINX-MD',
+                    newsletterJid: '120363418156081092@newsletter',
+                    newsletterName: 'Hᴀᴍɪᴅ Sʜᴀʜ',
                     serverMessageId: -1
                 }
             }
